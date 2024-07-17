230c21bf093ab08a6b77b9c723cd1432fa7526c1
dhanushvchekuri <124608859+dhanushvchekuri@users.noreply.github.com>
2024-07-17 16:29:03 -0700

    Made a responsive Simple interest calculator

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const submission = document.querySelector('#calcsimpleinterest')
+  const output = document.querySelector('#output')
+  submission.addEventListener('click', (event) => {
+    event.preventDefault()
+    let totalInterest = 0
+    const principal = document.getElementById('principal').value
+    let interest = document.getElementById('interest').value
+    let interestRate = interest / 100
+    const time = document.getElementById('time').value
+    totalInterest = principal * interestRate * time
+
+    const totalInterestText = document.createTextNode(
+      `Your total interest for principal of ${principal} with an interest rate of ${interest}% over the span of ${time} years is ${totalInterest}.`
+    )
+
+    // Create the line break element
+    const lineBreak = document.createElement('br')
+
+    const monthlyInterestText = document.createTextNode(
+      `Monthly: ${totalInterest / (time * 12)}`
+    )
+
+    // Append the text nodes and line break to the output element
+    output.appendChild(totalInterestText)
+    output.appendChild(lineBreak)
+    output.appendChild(monthlyInterestText)
+  })
+
+  const reset = document.querySelector('#resetinputs')
+  reset.addEventListener('click', (event) => {
+    event.preventDefault()
+    document.getElementById('principal').value = ''
+    document.getElementById('interest').value = ''
+    document.getElementById('time').value = ''
+    document.querySelector('#output').innerText = ''
+  })
+})
